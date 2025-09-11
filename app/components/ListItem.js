4e2873eee74f72ab4911404bf045f6101540145e
DESKTOP-F101LOH\DESKTOP <jose.baptistasjr@gmail.com>
2025-09-11 17:54:48 -0300

    extending the listItem component

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -6,17 +6,18 @@ import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
 import colors from '../config/colors';
 
 
-function ListItem({title, subTitle, image, onPress, renderRightActions}) {
+function ListItem({title, subTitle, image, ImageComponent, onPress, renderRightActions}) {
     return (
         <Swipeable renderRightActions={renderRightActions}>
         <TouchableHighlight 
         underlayColor={colors.light}
         onPress={onPress}>
             <View style={styles.container}>
-                <Image source={image} style={styles.image} />
-                <View>
+                {ImageComponent}
+                {image && <Image source={image} style={styles.image} />}
+                <View style={styles.detailsContainer}>
                     <AppText style={styles.title}>{title}</AppText>
-                    <AppText style={styles.subTitle}>{subTitle}</AppText>
+                    {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
                 </View>
             </View>
         </TouchableHighlight>
@@ -28,12 +29,15 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         padding: 15,  
-},
+    },
+    detailsContainer: {
+        marginLeft: 10,
+        justifyContent: 'center',
+    },
     image: {
         width: 70,
         height: 70,
         borderRadius: 35,
-        marginRight: 10,
     },
     title: {
         fontWeight: 'bold',
