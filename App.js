3272b045594bf8e3cfa395f0815e60121e5f84ba
DESKTOP-F101LOH\DESKTOP <jose.baptistasjr@gmail.com>
2025-09-11 02:54:32 -0300

    handling swipes

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+import AppButton from './app/components/AppButton';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
+
+export default function App() {
+  return (
+    <GestureHandlerRootView>
+      <MessagesScreen />
+    </GestureHandlerRootView>
+  );
+}
+
+
+
---
+++ app/components/ListItem.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, StyleSheet, Image, TouchableHighlight } from 'react-native';
+import AppText from './AppText';
+import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
+
+import colors from '../config/colors';
+
+
+function ListItem({title, subTitle, image, onPress, renderRightActions}) {
+    return (
+        <Swipeable renderRightActions={renderRightActions}>
+        <TouchableHighlight 
+        underlayColor={colors.light}
+        onPress={onPress}>
+            <View style={styles.container}>
+                <Image source={image} style={styles.image} />
+                <View>
+                    <AppText style={styles.title}>{title}</AppText>
+                    <AppText style={styles.subTitle}>{subTitle}</AppText>
+                </View>
+            </View>
+        </TouchableHighlight>
+        </Swipeable>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        padding: 15,  
+},
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+        marginRight: 10,
+    },
+    title: {
+        fontWeight: 'bold',
+    },
+    subTitle: {
+        color: colors.medium,
+    },
+})
+
+export default ListItem;
