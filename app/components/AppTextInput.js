f8696d455f694f834071786a92b67ef2d9b1cdce
DESKTOP-F101LOH\DESKTOP <jose.baptistasjr@gmail.com>
2025-09-28 18:47:34 -0300

    Building a pretty textinput, extracting the Default Styles

---
+++ app/components/AppTextInput.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, StyleSheet, Platform, TextInput, StatusBar} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+
+import defaultStyles from '../config/styles';
+
+function AppTextInput({icon, ...otherProps}) {
+    return (
+       <View style={styles.container}>
+        {icon && <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon} />}
+        <TextInput style={defaultStyles.text} {...otherProps}/>
+       </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: defaultStyles.colors.light,
+        borderRadius: 25,
+        flexDirection: 'row',
+        width: '100%',
+        padding: 15,
+        marginVertical: 20,
+        marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    },
+    icon: {
+        marginRight: 10,
+        alignSelf: 'center',
+    },
+})
+
+export default AppTextInput;
