496cdcba8bda05fe37ffda300513bb41c415f9e1
DESKTOP-F101LOH\DESKTOP <jose.baptistasjr@gmail.com>
2025-10-13 20:40:46 -0300

    handling selections

--- App.js
+++ App.js
@@ -17,18 +17,22 @@ import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
 import PickerItem from './app/components/PickerItem';
 
-export default function App() {
-  const categories = [
+const categories = [
     { label: 'Furniture', value: 1 },
     { label: 'Clothing', value: 2 },
     { label: 'Cameras', value: 3 },
   ]
 
+export default function App() {
+  const [category, setCategory] = useState(categories[0]);
 
   return (
     <GestureHandlerRootView>
       <Screen>
-        <AppPicker items={categories} icon="apps" placeholder='Category'/>
+        <AppPicker 
+          selectedItem={category}
+          onSelectItem={item => setCategory(item)} 
+          items={categories} icon="apps" placeholder='Category'/>
         <AppTextInput icon="email" placeholder='Email'/>
       </Screen>
     </GestureHandlerRootView>
