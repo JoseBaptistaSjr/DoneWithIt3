2b17f8638a6ed9da3a3e504adcba485fa20ce5c6
DESKTOP-F101LOH\DESKTOP <jose.baptistasjr@gmail.com>
2025-10-15 23:23:37 -0300

    build the errormessage component and the touched state

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -6,7 +6,7 @@ import * as Yup from 'yup'
 import Screen from '../components/Screen';
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
-import AppText from '../components/AppText';
+import ErrorMessage from '../components/ErrorMessage';
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
@@ -25,24 +25,26 @@ function LoginScreen(props) {
             onSubmit={values => console.log(values)}
             validationSchema={validationSchema}
           >
-            {({handleChange, handleSubmit, errors}) => (
+            {({handleChange, handleSubmit, errors, setFieldTouched, touched}) => (
               <>
                 <AppTextInput
                   autoCapitalize="none"
                   autoCorrect={false}
                   icon="email"
                   keyboardType="email-address"
+                  onBlur={() => setFieldTouched("email")}
                   onChangeText={handleChange("email")}
                   placeholder="Email"/>
-                <AppText style={{color: "red"}}>{errors.email}</AppText>    
+                <ErrorMessage error={errors.email} visible={touched.email}/>
                 <AppTextInput
                   autoCapitalize="none"
                   autoCorrect={false}
                   icon="lock"
+                  onBlur={() => setFieldTouched("password")}
                   onChangeText={handleChange("password")}
                   placeholder="Password"
                   secureTextEntry/>  
-                <AppText style={{color: "red"}}>{errors.password}</AppText>      
+                <ErrorMessage error={errors.password} visible={touched.password}/>     
                 <AppButton
                   title="Login"
                   onPress={handleSubmit}/>
