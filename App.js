92b303ecf33318ff062f49359c30dfed15b15b4c
DESKTOP-F101LOH\DESKTOP <jose.baptistasjr@gmail.com>
2025-11-12 19:52:23 -0300

    upgrade to Expo SDK 54 (fully working)

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import LoginScreen from './app/screens/LoginScreen';
+import AccountScreen from './app/screens/AccountScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
 
   return (
     <GestureHandlerRootView>
-      <LoginScreen />
+      <ListingDetailsScreen />
     </GestureHandlerRootView>
   );
 }
